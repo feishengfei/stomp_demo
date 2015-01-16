1fca777016027a746afb7f9f9e187948eee339e4
Frank Zhou <feishengfei@126.com>
2015-01-16 18:02:06 +0800

    add test

---
+++ src/test/java/jsonlib/test/JsonLibTest.java
@@ -0,0 +1,27 @@
+package jsonlib.test;
+
+import junit.framework.TestCase;
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
+import org.junit.After;
+import org.junit.Before;
+
+import com.frank.pojo.HeartbeatMessage;
+
+public class JsonLibTest extends TestCase{
+	
+	private HeartbeatMessage hm = null;
+	private JSONArray jsonArray = null;
+	private JSONObject jsonObject = null;
+	
+	@Before
+	public void init() {
+	}
+	
+	@After
+	public void destroy() {
+	
+	}
+	
+}
